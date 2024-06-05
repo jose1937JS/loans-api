6c00df069610cd641639ceee5247c317142b5666
Jose Lopez <jflo1937@gmail.com>
2024-06-05 00:42:24 -0400

    ADD service layer

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoanStoreRequest;
+use App\Http\Resources\LoanResource;
+use App\Services\LoanService;
 
 class LoanController extends Controller
 {
+    protected $loanService;
+    public function __construct(LoanService $loanService)
+    {
+        $this->loanService = $loanService;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -17,9 +26,10 @@ class LoanController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(LoanStoreRequest $request)
     {
-        //
+        return $loan = $this->loanService->store([]);
+        // return LoanResource::make($loan);
     }
 
     /**
---
+++ app/Services/LoanService.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Services;
+
+use App\Models\Loan;
+use App\Models\Refund;
+
+class LoanService
+{
+  public function store($data)
+  {
+    return 'guardando loan';
+  }
+}
